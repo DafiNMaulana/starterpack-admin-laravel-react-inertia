62462e29bd0397f8213b10337a2234d615920c68
DafiNMaulana <unoobcombro@gmail.com>
2023-06-12 14:31:02 +0700

    membuat component table

---
+++ resources/js/Components/Table.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import InputSearch from "@/Components/InputSearch";
+import { Link } from "@inertiajs/react";
+import BtnLinkSm from "@/Components/BtnLinkSm"
+
+export default function Table({children, children2}) {
+    return (
+        <div className="bg-white rounded-xl p-4">
+            <div className="flex justify-end gap-4">
+                <label htmlFor="search">
+                    <InputSearch />
+                </label>
+                <BtnLinkSm href="#" content="Tambah user"/>
+            </div>
+
+            <div className="overflow-x-auto mt-10">
+                <table className="table w-full">
+                    <thead>
+                        {children}
+                    </thead>
+                    <tbody>
+                        {children2}
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    );
+}
