0c9d9ae59270e8dd446ea83800956246be5da030
DafiNMaulana <unoobcombro@gmail.com>
2023-06-12 14:32:51 +0700

    bikin input search jadi component

---
+++ resources/js/Components/InputSearch.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { SearchNormal1 } from "iconsax-react";
+
+export default function InputSearch({className}) {
+    return (
+        <div htmlFor="search" className={`input-group input-group-sm  ${className}`}>
+            <button className="btn btn-sm bg-base-100 hover:bg-base-100 border-none rounded-r-none">
+                <SearchNormal1
+                    variant="Linear"
+                    className="text-[#C7C3C3] w-4"
+                />
+            </button>
+            <input
+                type="text"
+                placeholder="Cari anime kesayangan disini ..."
+                className="input input-sm rounded-r-lg placeholder:font-normal outline-none border-none focus:ring-0 focus:ring-offset-0 focus:outline-none w-[350px]"
+            />
+        </div>
+    );
+}
