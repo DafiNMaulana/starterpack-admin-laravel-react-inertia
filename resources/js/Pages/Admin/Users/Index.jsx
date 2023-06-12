fe37a636166e94c9280b0da64da7082aec96cbbd
DafiNMaulana <unoobcombro@gmail.com>
2023-06-12 14:30:34 +0700

    Memisahkan th dan td jadi children dan children2

--- resources/js/Pages/Admin/Users/Index.jsx
+++ resources/js/Pages/Admin/Users/Index.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
-import {Head} from '@inertiajs/react'
-import Sidebar from '@/Components/Sidebar'
+import React from "react";
+import { Head } from "@inertiajs/react";
+import Sidebar from "@/Components/Sidebar";
+import Table from "@/Components/Table";
 
 export default function Index() {
-
     return (
         <Sidebar>
             <Head>
                 <title>Users</title>
             </Head>
-            <div className="flex flex-col items-center justify-center">
-                <h1 className="text-3xl font-bold text-gray-700">Users</h1>
-            </div>
+            <Table>
+                <tr>
+                    <th className="text-center">Nama</th>
+                    <th className="text-center">Email</th>
+                    <th className="text-center">Role</th>
+                    <th className="text-center">Aksi</th>
+                </tr>
+                <tr>
+                    <td className="text-center">Admin</td>
+                    <td className="text-center">Admin@kw.com</td>
+                    <td className="text-center">Jluu</td>
+                    <td className="text-center flex gap-3 justify-center items-center">
+                        <button className="btn btn-sm btn-warning">Edit</button>
+                        <button className="btn btn-sm btn-error">Hapus</button>
+                    </td>
+                </tr>
+            </Table>
         </Sidebar>
-    )
+    );
 }
